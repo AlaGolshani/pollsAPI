448aab7455a405a2bb258fd61943d6fee953ea07
ala.golshani2001@gmail.com <ala.golshani2001@gmail.com>
2021-07-30 17:53:35 +0430

    Changing the views

--- polls/urls.py
+++ polls/urls.py
@@ -4,10 +4,15 @@ from .views import PollList, PollDetail
 
 urlpatterns = [
     # path("polls/", polls_list, name="polls_list"),  # step 1
+
     path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),  # step 2
 
     path("polls/", PollList.as_view(), name="polls_list"),  # step 3, Using generics
-    path("choices/", ChoiceList.as_view(), name="choice_list"),
-    path("vote/", CreateVote.as_view(), name="create_vote"),
+    # path("choices/", ChoiceList.as_view(), name="choice_list"),
+    # path("vote/", CreateVote.as_view(), name="create_vote"),
 
+    path("polls/<int:pk>/choices/", ChoiceList.as_view(),  # step 4, Changing the views
+         name="choice_list"),
+    path("polls/<int:pk>/choices/<int:choice_pk>/vote/",
+         CreateVote.as_view(), name="create_vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, status
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
 from rest_framework.views import APIView
@@ -54,10 +54,34 @@ class PollDetail(generics.RetrieveDestroyAPIView):
     serializer_class = PollSerializer
 
 
+# class ChoiceList(generics.ListCreateAPIView):
+#     queryset = Choice.objects.all()
+#     serializer_class = ChoiceSerializer
+#
+#
+# class CreateVote(generics.CreateAPIView):
+#     serializer_class = VoteSerializer
+
+
+# step 4, Changing the views
+
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs["pk"])
+        return queryset
+
     serializer_class = ChoiceSerializer
 
 
-class CreateVote(generics.CreateAPIView):
+class CreateVote(APIView):
     serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get("voted_by")
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
