9b2903b5f34797b16c9336a2e32d96f04c9373f3
ala.golshani2001@gmail.com <ala.golshani2001@gmail.com>
2021-07-30 16:54:34 +0430

    Using DRF generic views

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,9 @@ from .views import polls_list, polls_detail
 from .views import PollList, PollDetail
 
 urlpatterns = [
-    path("polls/", polls_list, name="polls_list"),
-    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),
+    # path("polls/", polls_list, name="polls_list"),  # step 1
+    path("polls/<int:pk>/", PollDetail.as_view(), name="polls_detail"),  # step 2
+
+    path("polls/", PollList.as_view(), name="polls_list"),  # step 3, Using generics
+
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,13 @@
+from rest_framework import generics
 from django.shortcuts import get_object_or_404
-
 from django.http import JsonResponse
-from .models import Poll, Choice
-
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import PollSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from .models import Poll, Choice
 
 
-# step 1
+# step 1, FBV
 
 def polls_list(request):
     MAX_OBJECTS = 20
@@ -27,17 +26,29 @@ def polls_detail(request, pk):
     return JsonResponse(data)
 
 
-# step 2
+# step 2, Using API
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+# class PollList(APIView):
+#     def get(self, request):
+#         polls = Poll.objects.all()[:20]
+#         data = PollSerializer(polls, many=True).data
+#         return Response(data)
+#
+#
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         poll = get_object_or_404(Poll, pk=pk)
+#         data = PollSerializer(poll).data
+#         return Response(data)
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-        return Response(data)
+# step 3, Using DRF generic
+
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
